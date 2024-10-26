86a50f768d9a275debbb4a077ec7805341b6fb34
Wesley Hu <91095436+weswes2EPYC@users.noreply.github.com>
2024-10-26 12:59:05 -0400

    temp

--- frontend/src/pages/Manage.jsx
+++ frontend/src/pages/Manage.jsx
@@ -1,8 +1,28 @@
+import { Grid, Card, Text, Button, Group } from '@mantine/core';
+
 function Manage() {
     return (
-        <div>
-            
+        <div className="p-4">
+            <h1 className="text-left text-2xl font-bold mb-4">Current Projects</h1>
+            <Grid>
+                <Grid.Col span={4}>
+                    <Card shadow="sm" padding="lg" radius="md" withBorder>
+                        <Text weight={500} size="lg" className="mb-2">
+                            Project Title
+                        </Text>
+
+                        <Text size="sm" color="dimmed" className="mb-4">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe rerum quia qui rem expedita nobis, autem id praesentium nam quaerat quo itaque assumenda. Voluptates omnis molestias reiciendis id accusamus aliquam!
+                        </Text>
+
+                        <Button variant="light" color="blue" fullWidth>
+                            View Project
+                        </Button>
+                    </Card>
+                </Grid.Col>
+            </Grid>
         </div>
+        
     );
 }
 
